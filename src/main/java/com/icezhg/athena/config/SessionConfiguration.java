d118fb7d14ea40cf525a45067e9654fe1c92e8f9
zhongjibing <zhongjibing@163.com>
2022-08-23 17:28:29 +0800

    opt

--- src/main/java/com/icezhg/athena/config/SessionConfiguration.java
+++ src/main/java/com/icezhg/athena/config/SessionConfiguration.java
@@ -1,12 +1,12 @@
 package com.icezhg.athena.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by zhongjibing on 2021/01/16
  */
 @Configuration
-@EnableJdbcHttpSession
+@EnableRedisHttpSession
 public class SessionConfiguration {
 }
