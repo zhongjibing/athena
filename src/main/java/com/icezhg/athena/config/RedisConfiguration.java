e7ee7f49587f019678964d40f95f4b5ad95bf75a
zhongjibing <zhongjibing@163.com>
2022-08-30 21:38:25 +0800

    remove duplicates

--- src/main/java/com/icezhg/athena/config/RedisConfiguration.java
+++ src/main/java/com/icezhg/athena/config/RedisConfiguration.java
@@ -1,7 +1,7 @@
 package com.icezhg.athena.config;
 
 
-import com.icezhg.athena.util.ObjectMapperFactory;
+import com.icezhg.authorization.core.util.ObjectMapperFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
--- src/main/java/com/icezhg/athena/controller/UserController.java
+++ src/main/java/com/icezhg/athena/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.icezhg.athena.controller;
 
 import com.icezhg.athena.service.UserService;
-import com.icezhg.athena.util.SecurityUtil;
+import com.icezhg.authorization.core.SecurityUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
