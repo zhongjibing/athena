7394419bbc000b4a521d7c625e5a29a5b327d17e
zhongjibing <zhongjibing@163.com>
2022-09-17 02:56:07 +0800

    add cache support

--- src/main/java/com/icezhg/athena/config/RedisConfiguration.java
+++ src/main/java/com/icezhg/athena/config/RedisConfiguration.java
@@ -2,13 +2,22 @@ package com.icezhg.athena.config;
 
 
 import com.icezhg.authorization.core.util.ObjectMapperFactory;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
+@EnableCaching
 @Configuration
 public class RedisConfiguration {
 
@@ -29,5 +38,19 @@ public class RedisConfiguration {
         Jackson2JsonRedisSerializer<Object> defaultSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         defaultSerializer.setObjectMapper(ObjectMapperFactory.getObjectMapper());
         return defaultSerializer;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer.UTF_8))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(springSessionDefaultRedisSerializer()))
+                        .disableCachingNullValues()
+                        .prefixCacheNameWith("athena:")
+                        .entryTtl(Duration.ofMinutes(30L))
+                )
+                .build();
     }
 }
