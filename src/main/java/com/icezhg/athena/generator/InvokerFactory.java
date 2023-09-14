8812befe632397677bf1e2a5a69c3c8a9701050a
zhongjibing <zhongjibing@163.com>
2023-09-15 00:48:39 +0800

    add generator

---
+++ src/main/java/com/icezhg/athena/generator/InvokerFactory.java
@@ -0,0 +1,16 @@
+package com.icezhg.athena.generator;
+
+import com.icezhg.generator.config.Config;
+import com.icezhg.generator.handler.FileWriterResultHandler;
+import com.icezhg.generator.invoker.Invoker;
+import com.icezhg.generator.invoker.SimpleInvoker;
+
+/**
+ * Created by zhongjibing on 2023/09/15.
+ */
+public class InvokerFactory {
+
+    public static Invoker simpleInvoker(Config config) {
+        return new SimpleInvoker(config, new FileWriterResultHandler());
+    }
+}
