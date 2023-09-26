a38d39d5719e766ac5bfa776daa94bc0c0f0aa5a
zhongjibing <zhongjibing@163.com>
2023-09-26 14:42:59 +0800

    passwd generator opt

--- src/main/java/com/icezhg/athena/util/PasswdGenerator.java
+++ src/main/java/com/icezhg/athena/util/PasswdGenerator.java
@@ -8,14 +8,13 @@ import java.util.Random;
  */
 public class PasswdGenerator {
 
-    private static final char[][] DICT = {
-            {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'},
-            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                    'v', 'w', 'x', 'y', 'z'},
-            {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                    'V', 'W', 'X', 'Y', 'Z'},
-            {'`', '@', '#', '%', '^', '&', '_', ';', '?', '-', '=', '+'}
-    };
+    private static final char[] DIGITAL = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    private static final char[] LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private static final char[] UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    private static final char[] SYMBOL = {'`', '@', '#', '%', '^', '&', '_', ';', '.', ',', '?'};
+    private static final char[][] DICT = {DIGITAL, LOWERCASE, UPPERCASE, SYMBOL};
 
     private static final int CHECK = (1 << DICT.length) - 1;
 
